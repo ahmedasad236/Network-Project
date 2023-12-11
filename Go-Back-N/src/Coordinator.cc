d3db58886c6b608e47ed9d4f0b1d90a8f71a0197
ahmedasad236 <ahmedasad236@gmail.com>
2023-12-11 11:55:10 +0200

    Create the custom message

--- Go-Back-N/src/Coordinator.cc
+++ Go-Back-N/src/Coordinator.cc
@@ -19,10 +19,53 @@ Define_Module(Coordinator);
 
 void Coordinator::initialize()
 {
-    // TODO - Generated method body
+    std::string line;
+    std::ifstream filestream("../src/coordinator.txt");
+    if(!filestream.is_open()) return;
+
+
+
+    while (getline(filestream, line)) {
+
+            int* data;
+            data = readInput(line);
+            int nodeId = data[0]; //atoi(line.c_str());
+            int startTime = data[1]; //atoi(line.c_str() + 1);
+
+            EV << "nodeNumber From Coordinator = " << nodeId << endl;
+            EV << "startingTime From Coordinator  = " << startTime << endl;
+
+            cMessage *msg = new cMessage("Coordinator start message");
+            msg->addPar("nodeId");
+            msg->par("nodeId").setLongValue(nodeId);
+
+            msg->addPar("startTime");
+            msg->par("startTime").setLongValue(startTime);
+
+            if(nodeId == 0)
+                send(msg, "out_ports", 0);
+            else if(nodeId == 1)
+                send(msg, "out_ports", 1);
+    }
+
+    filestream.close();
 }
 
 void Coordinator::handleMessage(cMessage *msg)
 {
     // TODO - Generated method body
+}
+
+int* readInput(std::string s, std::string del= " ") {
+    int start, end = -1*del.size(), i = 0;
+    int data [2];
+
+    do {
+        start = end + del.size();
+        end = s.find(del, start);
+        data[i] = atoi(s.substr(start, end - start).c_str());
+        i++;
+    } while (end != -1);
+
+    return data;
 }
