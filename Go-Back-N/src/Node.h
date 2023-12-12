cc1f919c74d88ac00f2d7f6129e38248983487b2
ahmedasad236 <ahmedasad236@gmail.com>
2023-12-12 16:49:26 +0200

    Add error codes and modes of operation

--- Go-Back-N/src/Node.h
+++ Go-Back-N/src/Node.h
@@ -17,12 +17,51 @@
 #define __GO_BACK_N_NODE_H_
 
 #include <omnetpp.h>
-
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include "MyMessage_m.h"
 using namespace omnetpp;
 
-/**
- * TODO - Generated class
- */
+enum CODES {
+    NO_ERRORs,
+    DELAY,
+    DUP,
+    DUP_DELAY,
+    LOSS,
+    LOSS_BOTH,
+    MOD,
+    MOD_DELAY,
+    MOD_DUP,
+    MOD_DUP_DELAY,
+};
+
+std::map<std::string, CODES> error_codes = {
+        {"0000", NO_ERRORs},
+        {"0001", DELAY},
+        {"0010", DUP},
+        {"0011", DUP_DELAY},
+        {"0100", LOSS},
+        {"0101", LOSS},
+        {"0110", LOSS_BOTH},
+        {"0111", LOSS_BOTH},
+        {"1000", MOD},
+        {"1001", MOD_DELAY},
+        {"1010", MOD_DUP},
+        {"1011", MOD_DUP_DELAY},
+        {"1100", LOSS},
+        {"1101", LOSS},
+        {"1110", LOSS_BOTH},
+        {"1111", LOSS_BOTH},
+};
+
+enum LOG_INFO_TYPE {
+    UPON_READING,
+    BEFORE_TRANS,
+    TIMEOUT_EVENT,
+    CONTROL_FRAME,
+};
+
 class Node : public cSimpleModule
 {
   protected:
